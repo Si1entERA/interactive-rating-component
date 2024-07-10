2010687f5ce5e8312cbe10179681dfc01440deae
Si1entERA <NeoLegote@outlook.com>
2024-07-10 19:55:28 +0200

    refactor code plus layout changes

--- main.js
+++ main.js
@@ -6,32 +6,33 @@ const thankYouTop = document.querySelector('.thank-you-top');
 const thankYouMiddle = document.querySelector('.thank-you-middle');
 const thankYouBottom = document.querySelector('.thank-you-bottom');
 
-
 const button = document.querySelectorAll('.btn');
 const submit = document.querySelector('.submit');
 const output = document.querySelector('.output');
 
 
-
 button.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     button.forEach((btn) => btn.classList.remove('active'));
-    // button.forEach((btn) => btn.style.backgroundColor = 'var(--Dark-Blue)');
     e.currentTarget.classList.add('active'); 
-    
-  output.innerHTML = e.currentTarget.innerHTML
-
+    output.innerHTML = e.currentTarget.innerHTML
   })
 })
 
 
-submit.addEventListener('click', (e) => {
+const removeRatingPage = () => {
   topSection.style.display = 'none'
   middleSection.style.display = 'none'
   bottomSection.style.display = 'none'
+}
 
+const addThankYouState = () => {
   thankYouTop.style.display = 'flex'
   thankYouMiddle.style.display = 'block'
   thankYouBottom.style.display = 'flex'
+}
 
+submit.addEventListener('click', () => {
+  removeRatingPage();
+  addThankYouState();
 })
