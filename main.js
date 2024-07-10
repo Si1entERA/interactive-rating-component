75fbcdaab267e2d20903cdd89255211396b47af7
Si1entERA <NeoLegote@outlook.com>
2024-07-10 16:07:33 +0200

    code added with spacing changes

---
+++ main.js
@@ -0,0 +1,37 @@
+const topSection = document.querySelector('.top');
+const middleSection = document.querySelector('.middle');
+const bottomSection = document.querySelector('.bottom');
+
+const thankYouTop = document.querySelector('.thank-you-top');
+const thankYouMiddle = document.querySelector('.thank-you-middle');
+const thankYouBottom = document.querySelector('.thank-you-bottom');
+
+
+const button = document.querySelectorAll('.btn');
+const submit = document.querySelector('.submit');
+const output = document.querySelector('.output');
+
+
+
+button.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    button.forEach((btn) => btn.classList.remove('active'));
+    // button.forEach((btn) => btn.style.backgroundColor = 'var(--Dark-Blue)');
+    e.currentTarget.classList.add('active'); 
+    
+  output.innerHTML = e.currentTarget.innerHTML
+
+  })
+})
+
+
+submit.addEventListener('click', (e) => {
+  topSection.style.display = 'none'
+  middleSection.style.display = 'none'
+  bottomSection.style.display = 'none'
+
+  thankYouTop.style.display = 'flex'
+  thankYouMiddle.style.display = 'block'
+  thankYouBottom.style.display = 'flex'
+
+})
